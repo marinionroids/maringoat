96f8d2051110f70a89c33a5b1ff2a3a826cf29bb
Marin <m-menkshi@hvcc.edu>
2026-02-04 02:13:56 +0100

    Rebrand: A eshte Marini programues me i mire se Matiasi?

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Je i sigurt?",
+    "Vertet je i sigurt??",
+    "Mendo pak...",
+    "Kush shkruan kod me mire? 🤔",
+    "Marini ka 0 bugs!",
+    "Matiasi ka stack overflow...",
+    "Marini kodon ne gjume!",
+    "Prano te verteten...",
+    "Ok, do ndalem...",
+    "Jo jo, shtyp Po! 💻🔥"
 ];
 
 let messageIndex = 0;
